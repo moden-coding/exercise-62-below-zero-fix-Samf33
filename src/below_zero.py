292f57b99cdae478a7f70e2906c4ffbb1fe728a5
Samf33 <144719150+Samf33@users.noreply.github.com>
2025-10-27 09:05:52 -0400

    done

--- src/below_zero.py
+++ src/below_zero.py
@@ -3,8 +3,8 @@
 import pandas as pd
 
 def below_zero():
-    pass
-
+    df = pd.read_csv("src/kumpula-weather-2017.csv", index_col=0)
+    return len(df[df["Air temperature (degC)"] < 0])
 def main():
     result = below_zero()
     print(f"Number of days below zero: {result}")
